1197400284929acf2a17b465edc55676308c97ef
poudlesuman32-star <poudle.suman32@gmail.com>
2026-08-08 13:28:53 -0500

    Bind R2 collector to migrated producer identity

--- bootstrap/ppi-data-acquisition/src/collect_raw_provider_evidence_r2.py
+++ bootstrap/ppi-data-acquisition/src/collect_raw_provider_evidence_r2.py
@@ -3,11 +3,15 @@ from __future__ import annotations
 
 import collect_raw_provider_evidence as collector
 
+PUBLIC_REPOSITORY = "MarketMakingLFG/ppi-data-acquisition"
+PUBLIC_REPOSITORY_ID = 1312286476
 PUBLIC_CONTRACT_ID = "PPI-R11-PUBLIC-ACQUISITION-003-R2"
 COLLECTOR_RELEASE_ID = "PPI-PUBLIC-COLLECTOR-003-R2"
 
 
 def main() -> int:
+    collector.PUBLIC_REPOSITORY = PUBLIC_REPOSITORY
+    collector.PUBLIC_REPOSITORY_ID = PUBLIC_REPOSITORY_ID
     collector.PUBLIC_CONTRACT_ID = PUBLIC_CONTRACT_ID
     collector.COLLECTOR_RELEASE_ID = COLLECTOR_RELEASE_ID
     return collector.main()
